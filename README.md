bd7a568be4905cec0dc772bfce3c8a19241995c9
Gert Massheimer <deltaos@web.de>
2019-04-06 17:20:52 -0400

    Better error handling

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
 # macOS Version with Uptime
 
 Übersicht widget to display the macOS name, version and build on the desktop.  
-Additionally it shows the system uptime.
-The Build-ID can be hidden and the widget is themable.
+Additionally it shows the system uptime.  
+The Build number can be hidden and the widget is themable.  
 Five themes are included: mono, paper, dark, myDark and color.
 
+This is my second version. This time it is written using REACT.
+
 ## Screenshots
-
-Theme: myDark  
-![Theme: myDark][image-1]  
-
-Theme: dark  
+Theme: myDark
+![Theme: myDark][image-1]
+Theme: dark
 ![Theme: dark][image-2]
 
-## Settings
+## Version
+2.0.1
 
-- Set the theme with the variable **theme** in the lines **18** - **22**. 
-- Turn the display of the Build-ID (18D42) on or off with the variable **showBuild** in the lines **26** and **27**.
-- Set the location of the widget in lines **31** and **32**.
+## History
+- 2.0.1 better error handling
+- 2.0   updated to REACT
+- 1.0   written in CoffeeScript  
 
 ## Acknowledgement
 
-This widget is based on Mike Panella's "[OS Version Pro](https://github.com/mpen01/os-version-pro)" widget. 
+This widget is mostly based on Mike Panella's "[OS Version Pro](https://github.com/mpen01/os-version-pro)" widget. 
 
 [image-1]:screenshots/myDark.png?raw=true
 [image-2]:screenshots/dark.png?raw=true
--- index.jsx
+++ index.jsx
@@ -1,24 +1,26 @@
 /*
   Name:         os-version-uptime.widget
   Description:  Shows OS-Version and Up-Time for Übersicht
-  Created:      28.Jan.2019
+  Created:      05.Apr.2019
   Author:       Gert Massheimer
-  Version:      2.0
-  Note:         This version is updated to REACT while version 1.0 was written in CoffeeScript
-
- Based on:
+  Version:      2.0.1
+  History:      2.0.1 better error handling
+                2.0   updated to REACT
+                1.0   written in CoffeeScript
+  Based on:
    OS Version Pro for Übersicht
    By Mike Pennella
    https://github.com/mpen01/os-version-pro
 -------------------------------------------------------------------------*/
+'use strict';
 
 // Change the theme variable below to style the widget
 // THEME OPTIONS: mono, paper, color, dark or myDark
 
 // const theme = 'mono';
 // const theme = 'paper';
- const theme = 'color';
-//const theme = 'dark';
+// const theme = 'color';
+const theme = 'dark';
 //const theme = 'myDark'; // color theme with dark icon;
 /*-----------------------------------------------------------------------*/
 
@@ -69,95 +71,115 @@ if (theme === 'dark') {
 /*=== DO NOT EDIT AFTER THIS LINE unless you know what you're doing! ===
 ========================================================================*/
 
-import { css } from "uebersicht";
-import { styled } from "uebersicht";
+import { css, styled } from "uebersicht";
 
 export const refreshFrequency = 60000; // every 60000ms = every minute
+
 export const command = "os-version-uptime.widget/data.sh";
 
-export const updateState = (event) => {
-  return event.output.split(/\r\n|\r|\n/g); // splitting at linebreak
+export const updateState = (event, previousState) => {
+  if (event.error) {
+    return { ...previousState, warning: `We got an error: ${event.error}` };
+  }
+  const [OS_Version, UP_Time] = event.output.split(/\r\n|\r|\n/g); // splitting at linebreak
+  return { OS_Version, UP_Time };
 };
 
-export const render = (output) => {
+export const render = ({OS_Version, UP_Time, error}) => {
 
-  const os = output[0]; // OS-Version part
-  const up = output[1]; // Up-Time part
+  if (error) return (
+    <div>Something went wrong: <strong>{String(error)}</strong></div>
+  );
+  else {
 
-  /* --- Create the up-time string --------------------------------------*/
-  const upValues  = up.split(" ");
-  const up0 = upValues[0];
-  const up1 = upValues[1];
-  const up2 = upValues[2] ? upValues[2] : '';
-  const up3 = upValues[3] ? upValues[3] : '';
-  const up4 = upValues[4] ? upValues[4] : '';
-  const up5 = upValues[5] ? upValues[5] : '';
-  const uptime = up0 + ' ' + up1 + ' ' + up2 + ' ' + up3 + ' ' + up4 + ' ' + up5;
+    /* --- Create the up-time string --------------------------------------*/
+    const upValues = UP_Time.split(" ");
+    const up0 = upValues[0];
+    const up1 = upValues[1];
+    const up2 = upValues[2] ? upValues[2] : '';
+    const up3 = upValues[3] ? upValues[3] : '';
+    const up4 = upValues[4] ? upValues[4] : '';
+    const up5 = upValues[5] ? upValues[5] : '';
+    const uptime = up0 + ' ' + up1 + ' ' + up2 + ' ' + up3 + ' ' + up4 + ' ' + up5;
 
-  /* --- Create the OS information --------------------------------------*/
-  const osValues  = os.split(" ");
-  let   osName    = osValues[0];
-  const osVersion = osValues[1];
-  const osBuild   = osValues[2];
+    /* --- Create the OS information --------------------------------------*/
+    const osValues  = OS_Version.split(" ");
+    let   osName    = osValues[0];
+    const osVersion = osValues[1];
+    const osBuild   = osValues[2];
 
-  const iconDir = 'os-version-uptime.widget/icons/';
+    const iconDir = 'os-version-uptime.widget/icons/';
 
-  let icon, osRelease;
-  switch (osVersion.substr(0,5)) {
-    case '10.10': icon = iconDir + "yosemite.png";    osRelease = ' Yosemite';    break;
-    case '10.11': icon = iconDir + "el_capitan.png";  osRelease = ' El Capitan';  break;
-    case '10.12': icon = iconDir + "sierra.png";      osRelease = ' Sierra';      break;
-    case '10.13': icon = iconDir + "high_sierra.png"; osRelease = ' High Sierra'; break;
-    case '10.14':
-      if (theme === 'dark' || theme === 'myDark') icon = iconDir + "mojave_dark.png";
-      else icon = iconDir + "mojave.png";
-      osRelease = ' Mojave';
-      break;
-    default: icon = iconDir + "mac_os.png";
-  }
-
-  if (osName === 'OSX')
-    osName = osName.substr(0,2) + " " + osName.substr(2,1);
-
-  /* --- Put the html together ------------------------------------------*/
-  const System = () => {
-    if (osName !== '') return (
-      <div className={nameDiv}>
-        <span className={css_osName}>{osName}</span>
-        <span className={css_osRelease}>{osRelease}</span>
-      </div>);
-  };
-
-  const Version = () => {
-    if (osName !== '') {
-      if (showBuild === true)
-        return (<p className={css_osVersion}>Version {osVersion} {osBuild}</p>);
-      else
-        return (<p className={css_osVersion}>Version {osVersion}</p>);
+    let icon, osRelease;
+    switch (osVersion.substr(0, 5)) {
+      case '10.10':
+        icon = iconDir + "yosemite.png";
+        osRelease = ' Yosemite';
+        break;
+      case '10.11':
+        icon = iconDir + "el_capitan.png";
+        osRelease = ' El Capitan';
+        break;
+      case '10.12':
+        icon = iconDir + "sierra.png";
+        osRelease = ' Sierra';
+        break;
+      case '10.13':
+        icon = iconDir + "high_sierra.png";
+        osRelease = ' High Sierra';
+        break;
+      case '10.14':
+        if (theme === 'dark' || theme === 'myDark') icon = iconDir + "mojave_dark.png";
+        else icon = iconDir + "mojave.png";
+        osRelease = ' Mojave';
+        break;
+      default:
+        icon = iconDir + "mac_os.png";
     }
-  };
 
-  const NoName = () => {
-    if (osName === '') return (
-      <div className={noNameDiv}>
-        <span className={css_osVersion}>OS info is not available</span>
-      </div>);
-  };
+    if (osName === 'OSX')
+      osName = osName.substr(0, 2) + " " + osName.substr(2, 1);
 
-  /* --- Display the widget ---------------------------------------------*/
-  if (osName !== '') return (
-    <Div>
-      <div><Img src={icon} alt=''/></div>
-      <System/>
-      <Version/>
-      <p className={css_uptime}>{uptime}</p>
-    </Div>);
-  else return (
-    <Div>
-      <div><Img src={icon} alt=''/></div>
-      <NoName/>
-      <p className={css_uptime}>{uptime}</p>
-    </Div>);
+    /* --- Put the html together ------------------------------------------*/
+    const System = () => {
+      if (osName !== '') return (
+        <div className={nameDiv}>
+          <span className={css_osName}>{osName}</span>
+          <span className={css_osRelease}>{osRelease}</span>
+        </div>);
+    };
+
+    const Version = () => {
+      if (osName !== '') {
+        if (showBuild === true)
+          return (<p className={css_osVersion}>Version {osVersion} {osBuild}</p>);
+        else
+          return (<p className={css_osVersion}>Version {osVersion}</p>);
+      }
+    };
+
+    const NoName = () => {
+      if (osName === '') return (
+        <div className={noNameDiv}>
+          <span className={css_osVersion}>OS info is not available</span>
+        </div>);
+    };
+
+    /* --- Display the widget ---------------------------------------------*/
+    if (osName !== '') return (
+      <Div>
+        <div><Img src={icon} alt=''/></div>
+        <System/>
+        <Version/>
+        <p className={css_uptime}>{uptime}</p>
+      </Div>);
+    else return (
+      <Div>
+        <div><Img src={icon} alt=''/></div>
+        <NoName/>
+        <p className={css_uptime}>{uptime}</p>
+      </Div>);
+  }
 };
 
 /* --- Widget styles ----------------------------------------------------*/
